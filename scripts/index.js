89f2e209d48413cb0158fc0d56b282309100c9f9
Dmitriy Trifonov <dimatriff@yandex.ru>
2024-12-04 16:30:11 +0700

    First iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+function createCard(cardData) {
+    
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true).querySelector('.places__item');
+  
+    
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+  
+    
+    deleteButton.addEventListener('click', () => {
+      cardElement.remove();
+    });
+  
+    return cardElement;
+  }
 
-// @todo: DOM узлы
+  const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+    placesList.append(cardElement);
+});
